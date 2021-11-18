e13370fd8d0e21d65f8e78623b25ec376c224bd3
matt <matt@thinkhappymedium.com>
2021-11-18 12:52:05 -0500

    Initial Commit

---
+++ RealmTest/RealmTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmTestApp.swift
+//  RealmTest
+//
+//  Created by Matt Gannon on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct RealmTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
